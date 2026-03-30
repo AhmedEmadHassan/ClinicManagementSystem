f100527566254748e9cdf76fb129e28b1aea4769
AhmedEmadHassan <104726251+AhmedEmadHassan@users.noreply.github.com>
2026-03-30 17:02:01 +0200

    Fixed Issues in DTOs

--- ClinicManagementSystem.Application/DTOs/CreateDTOs/Abstract/CreatePersonDTO.cs
+++ ClinicManagementSystem.Application/DTOs/CreateDTOs/Abstract/CreatePersonDTO.cs
@@ -2,7 +2,6 @@
 {
     public abstract class CreatePersonDTO
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
         public bool Gender { get; set; }
--- ClinicManagementSystem.Application/DTOs/ResponseDTOs/ResponseDoctorDTO.cs
+++ ClinicManagementSystem.Application/DTOs/ResponseDTOs/ResponseDoctorDTO.cs
@@ -1,8 +1,8 @@
-﻿using ClinicManagementSystem.Application.DTOs.CreateDTOs.Abstract;
+﻿using ClinicManagementSystem.Application.DTOs.ResponseDTOs.Abstract;
 
 namespace ClinicManagementSystem.Application.DTOs.ResponseDTOs
 {
-    public class ResponseDoctorDTO : CreatePersonDTO
+    public class ResponseDoctorDTO : ResponsePersonDTO
     {
         public int DoctorSpecializationId { get; set; }
         public string DoctorSpecializationName { get; set; } = string.Empty;
